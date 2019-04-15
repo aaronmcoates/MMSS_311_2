2c4a13af819ee9a579e7b7df05d2f1dcf264f043
Aaron Coates <acoates730@gmail.com>
2019-04-14 21:15:26 -0500

    finish lab 2

--- Lab 2.R
+++ Lab 2.R
@@ -1,5 +1,6 @@
 setwd("~/Documents/GitHub/MMSS_311_2")
 
+#1
 odev <- function(x){
   ifelse(round(x, digits=0) - x!=0, "Not an integer", ifelse(round(x/2, digits=0) - x/2 != 0, "Odd", "Even"))
 }
@@ -21,7 +22,7 @@ hey(5)
 hey(62)
 hey(34.5)
 
-
+#2
 vecky <- c(1:6)
 slayme <- c(sample(vecky, 20, replace = TRUE))
 
@@ -33,3 +34,29 @@ final <- sapply(slayme,
                   x^2
                 }})
 
+#3
+library(dplyr)
+data("starwars")
+head(starwars)
+
+print(dim(starwars))
+
+mean(starwars$mass, na.rm=TRUE)
+
+mean(starwars$mass[starwars$eye_color=="blue" & 160 < starwars$height & 200 > starwars$height], na.rm=TRUE)
+
+starwarz <- subset(starwars, starwars$eye_color=="blue" & 160 < starwars$height & 200 > starwars$height)
+
+newstars <- subset(starwars, select = c(name, height, mass, gender, species))
+
+plot(newstars$mass, newstars$height)
+
+newstars2 <- newstars[-c(16),]
+
+regression <- lm(newstars2$mass ~ newstars2$height)
+summary(regression)
+
+y <- newstars2$mass
+x <- newstars2$height
+reg <- lm(y ~ x)
+predict(reg, data.frame(x = 180))
