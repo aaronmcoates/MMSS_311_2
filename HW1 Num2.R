dfbbd07ea7b8f291c2b20ee7fa9637de18fa63ac
Aaron Coates <acoates730@gmail.com>
2019-05-03 20:05:13 -0500

    crying

--- HW1 Num2.R
+++ HW1 Num2.R
@@ -1,26 +1,89 @@
 setwd("~/Documents/GitHub/MMSS_311_2")
 
-milk <- read.csv('/Users/aaroncoates/Downloads/trumptweets.csv')
+TwitterData <- read.csv('/Users/aaroncoates/Downloads/trumptweets.csv')
 
 library(tidytext)
 library(tm)
 library(dplyr)
+library(broom)
+library(lubridate)
+library(stringr)
 
-corpusiii <- Corpus(VectorSource(as.vector(milk$text)))
+tweetcorpus <- Corpus(VectorSource(as.vector(TwitterData$text)))
 
-corpusiv <- corpusiii %>%
+processedcorpus <- tweetcorpus %>%
   tm_map(removeWords, stopwords("english")) %>%
   tm_map(content_transformer(tolower)) %>%
   tm_map(content_transformer(stemDocument), language = "english") %>%
   tm_map(content_transformer(removePunctuation))
 
-ahh <- DocumentTermMatrix(corpusiv)
-ahh2 <- removeSparseTerms(ahh, .99)
+DTMatrix <- DocumentTermMatrix(processedcorpus)
+SparseDTMatrix <- removeSparseTerms(DTMatrix, .99)
+inspect(SparseDTMatrix[1:10,1:10])
+tidymatrix <- tidy(PDTMatrix)
 
-inspect(ahh2[1:5,1:5])
+TfIdfMat <- DocumentTermMatrix(processedcorpus, control
+                          = list(weighting = weightTfIdf))
+SparseTfIdfMat <- removeSparseTerms(TfIdfMat, .99)
+inspect(SparseTfIdfMat[1:10,1:10])
 
-library(broom)
-ahh2 <- as.matrix(ahh2)
+popterms <- tidymatrix %>%
+  group_by(term) %>%
+  summarize(frequency = sum(count)) %>%
+  arrange(desc(frequency))
+popterms[1:20,1:2]
 
-ahh3 <- tidy(ahh2)
-ahh4 <- DocumentTermMatrix(corpusiv, control = list(weighting = weightTfIdf))
+TwitterDataDate <- TwitterData
+TwitterDataDate$date <- as.Date(TwitterData$created_at, '%m-%d-%Y')
+
+PostTwitterData <- subset(TwitterDataDate, date >= as.Date('2016-11-08'))
+PreTwitterData <- subset(TwitterDataDate, date <= as.Date('2016-11-08'))
+
+#post election
+posttweetcorpus <- Corpus(VectorSource(as.vector(PostTwitterData$text)))
+
+postprocessedcorpus <- posttweetcorpus %>%
+  tm_map(removeWords, stopwords("english")) %>%
+  tm_map(content_transformer(tolower)) %>%
+  tm_map(content_transformer(stemDocument), language = "english") %>%
+  tm_map(content_transformer(removePunctuation))
+
+postDTMatrix <- DocumentTermMatrix(postprocessedcorpus)
+postSparseDTMatrix <- removeSparseTerms(postDTMatrix, .99)
+inspect(postSparseDTMatrix[1:10,1:10])
+posttidymatrix <- tidy(postDTMatrix)
+
+postpopterms <- posttidymatrix %>%
+  group_by(term) %>%
+  summarize(frequency = sum(count)) %>%
+  arrange(desc(frequency))
+postpopterms[1:20,1:2]
+
+#pre election
+pretweetcorpus <- Corpus(VectorSource(as.vector(PreTwitterData$text)))
+
+preprocessedcorpus <- pretweetcorpus %>%
+  tm_map(removeWords, stopwords("english")) %>%
+  tm_map(content_transformer(tolower)) %>%
+  tm_map(content_transformer(stemDocument), language = "english") %>%
+  tm_map(content_transformer(removePunctuation))
+
+preDTMatrix <- DocumentTermMatrix(preprocessedcorpus)
+preSparseDTMatrix <- removeSparseTerms(preDTMatrix, .99)
+inspect(preSparseDTMatrix[1:10,1:10])
+pretidymatrix <- tidy(preDTMatrix)
+
+prepopterms <- pretidymatrix %>%
+  group_by(term) %>%
+  summarize(frequency = sum(count)) %>%
+  arrange(desc(frequency))
+prepopterms[1:20,1:2]
+
+#hashtags
+hashtagcorpus <- Corpus(VectorSource(as.vector(TwitterData$text)))
+
+processedhashtagcorpus <- hashtagcorpus %>%
+  tm_map(content_transformer(gsub), "[^\\#[:alnum:]\\s]", "")
+
+htDTMatrix <- DocumentTermMatrix(processedhashtagcorpus)
+tidy(htDTMatrix)
